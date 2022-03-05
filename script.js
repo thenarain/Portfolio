6c43d7608c5005a0ca7bc3cf34f21ae362176e71
Kartikay <kartiksrivastava19@gmail.com>
2022-03-05 16:44:58 +0100

    Fix Linter errors

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ const openMenu = document.querySelector('.open-menu');
 const closeMenu = document.querySelector('.close-menu');
 const logo = document.querySelector('.logo');
 const navBar = document.querySelector('.nav-bar');
-openMenu.addEventListener('click', show);
+
 
 function show() {
-    closeMenu.classList.toggle('hidden')
-    navBar.classList.add('show');
-    openMenu.classList.add('hidden')
-    mainMenu.classList.toggle('hidden');
-    logo.classList.add('hidden');
-    document.body.classList.add('no-scroll');
+  closeMenu.classList.toggle('hidden');
+  navBar.classList.add('show');
+  openMenu.classList.add('hidden');
+  mainMenu.classList.toggle('hidden');
+  logo.classList.add('hidden');
+  document.body.classList.add('no-scroll');
 }
 
-function closeMenuFun (){
-    openMenu.classList.remove('hidden')
-    closeMenu.classList.toggle('hidden')
-    navBar.classList.remove('show');
-    mainMenu.classList.toggle('hidden');
-    logo.classList.remove('hidden');
-    document.body.classList.remove('no-scroll');
+function closeMenuFun() {
+  openMenu.classList.remove('hidden');
+  closeMenu.classList.toggle('hidden');
+  navBar.classList.remove('show');
+  mainMenu.classList.toggle('hidden');
+  logo.classList.remove('hidden');
+  document.body.classList.remove('no-scroll');
 }
 
-
-closeMenu.addEventListener('click', closeMenuFun)
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', closeMenuFun);
